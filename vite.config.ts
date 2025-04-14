680222d9a69cd537fe2f6fa96368a9963fb27f79
onavi001 <34868568+onavi001@users.noreply.github.com>
2025-04-13 19:51:46 -0600

    update redirect config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Asegura que el build se genera en 'dist'
+    sourcemap: false, // Desactiva sourcemaps en producción para reducir tamaño
+  },
   server: {
     proxy: {
       "/api": {
@@ -14,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
